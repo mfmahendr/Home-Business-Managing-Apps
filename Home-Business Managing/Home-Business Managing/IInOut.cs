83fa0114ea3ad4d2ce1321658066b75d9557394c
M. Fadhil Mahendra <71614957+mfmahendr@users.noreply.github.com>
2021-06-04 19:56:18 +0700

    Mengganti file tanpa mengubah esensi aplikasi
    Menghapus direktori Home-Business-Managing

--- Home-Business Managing/Home-Business Managing/IInOut.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Home_Business_Managing
-{
-    public interface IInOut
-    {
-        // Innterface Seggregation Principle
-        void Login();
-        void Logout();
-    }
-}
--- Home-Business Managing/Home-Business Managing/IRegister.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Home_Business_Managing
-{
-    public interface IRegister
-    {
-        void SignUp();  // Innterface Seggregation Principle
-    }
-}
